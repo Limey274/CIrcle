b92c0ef30803c01435355ad5ef1145e2e0c40d92
Daniel <d.lalime7@gmail.com>
2020-06-02 12:52:52 -0700

    moe vs me

--- Program.cs
+++ Program.cs
@@ -8,11 +8,21 @@ namespace CIrcle
         {
             int r;
             double A;
-            Console.WriteLine("Enter the radius:");
+            Console.WriteLine("Enter the radius: ");
             r = Convert.ToInt32(Console.ReadLine());
             A = (Math.PI) * r * r;
             Console.WriteLine("The Area of circle = " + A);
             Console.WriteLine();
+
+            //double radius = double.Parse(Console.ReadLine());
+           // double area = Math.PI * radius * radius;
+           // double circumfrence = 2 * Math.PI * radius; 
+           // double diameter = 2 * radius
+
+
+           // Console.WriteLine($"\nFor a circle with a raduis of {radius}");
+            //Console.WriteLine($"The area is: {area}");
+            //Console.WriteLine($"The diameter is : {diameter}");
 
             double mpg;
             Console.WriteLine("What's your MPG? " );
