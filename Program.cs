cfbc99a04377273286e2e158883f76c1847b6783
Daniel <d.lalime7@gmail.com>
2020-06-02 10:40:00 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CIrcle
+{
+    class MathCircle
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            double A;
+            Console.WriteLine("Enter the radius:");
+            r = Convert.ToInt32(Console.ReadLine());
+            A = (Math.PI) * r * r;
+            Console.WriteLine("The Area of circle = " + A);
+            Console.WriteLine();
+
+            double mpg;
+            Console.WriteLine("What's your MPG? " );
+            mpg = Convert.ToDouble(Console.ReadLine());
+            while ( mpg <= 0 || mpg == double.NaN)
+            {
+                             
+                    Console.WriteLine(" This is not a vaild input");
+                    Console.WriteLine("What's your MPG? ");
+                    mpg = Convert.ToDouble(Console.ReadLine());
+              
+                 
+                
+            }
+            
+            Console.WriteLine("You will use " + A / mpg + " gallons of gas to travel the radius of the circle");
+
+        }
+    }
+}
